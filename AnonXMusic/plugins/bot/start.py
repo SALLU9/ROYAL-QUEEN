9236eec9d075ab0537dca055dcd294b4f9add01a
MOONLIGHTXD92 <moonlight92649@gmail.com>
2024-07-10 20:48:17 +0530

    Update start.py
    Signed-off-by: MOONLIGHTXD92 <moonlight92649@gmail.com>

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -68,8 +68,8 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_photo(
-                photo=config.START_IMG_URL,
+            return await message.reply_video(
+                random.choice(ANNIE_VID),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -135,8 +135,7 @@ async def start_pm(client, message: Message, _):
         await asyncio.sleep(2)
         await umm.delete()
         await message.reply_video(
-            video="https://telegra.ph/file/ac573bda31359a8bb51a5.mp4",
-                  "https://telegra.ph/file/c35eec6daf85bb743cec6.mp4",
+            random.choice(ANNIE_VID),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -152,8 +151,8 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_photo(
-        photo=config.START_IMG_URL,
+    await message.reply_video(
+        random.choice(ANNIE_VID),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
